6ee316a8be7da632087c648a68dab0c346a7d1a1
sL0p3 <shlokjakhotia@gmail.com>
2022-07-08 11:56:58 +0530

    adding buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             {props.title}
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,17 +5,50 @@ export default function TextForm(props) {
   const upperCaseClick = ()=>{
     setText(text.toUpperCase())
   }
+  const lowerCaseClick = ()=>{
+    setText(text.toLowerCase())
+  }
+  const clearClick = ()=>{
+    setText("")
+  }
+  const speakClick = ()=>{
+    let msg = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(msg);
+  }
+  const extraSpaceClick = ()=>{
+    setText((text.split(/[ ]+/)).join(" "))
+  }
+  const replaceClick = ()=>{
+    let toReplace = prompt("Enter text to be replaced : ")
+    toReplace = new RegExp(toReplace,'g')
+    let toReplaceWith = prompt("Enter the text you want to replace with : ")
+    setText(text.replace(toReplace,toReplaceWith))
+  }
   const handleOnChange = (event)=>{
     setText(event.target.value)
   }
   const [text,setText] = useState("");
   return (
-    <div>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-            <textarea className="form-control" value = {text} onChange = {handleOnChange} id="myBox" rows="8"></textarea>
+    <>
+        <div className="container">
+            <h1 className= "my-3" >{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control" value = {text} onChange = {handleOnChange} id="myBox" rows="8"></textarea>
+            </div>
+            <button className = "btn btn-warning mx-2" onClick={speakClick}>Speak</button>
+            <button className = "btn btn-danger mx-2" onClick={clearClick}>Clear</button>
+            <button className = "btn btn-primary mx-2" onClick={upperCaseClick}>Convert to UpperCase</button>
+            <button className = "btn btn-primary mx-2" onClick={lowerCaseClick}>Convert to LowerCase</button>
+            <button className = "btn btn-primary mx-2" onClick={replaceClick}>Change all occourances</button>
+            <button className = "btn btn-primary mx-2" onClick={extraSpaceClick}>Remove Extra Spaces</button>
         </div>
-        <button className = "btn btn-primary" onClick={upperCaseClick}>Convert to UpperCase</button>
-    </div>
+        <div className="container my-5">
+            <h3>Your text summary</h3>
+            <p>{text.split(" ").length } words and {text.length} characters.</p>
+            <p>Average time to read this text -- {0.008 * text.split(" ").length } minutes</p>
+            <h4>Preview</h4>
+            <p>{text}</p>
+        </div>
+    </>
   )
 }
