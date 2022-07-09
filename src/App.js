4aa4438a5336ed1a9626826c5ae9852d569001cf
sL0p3 <shlokjakhotia@gmail.com>
2022-07-09 08:35:22 +0530

    alerts added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,22 @@ import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 // import About from "./components/About";
 import React,{useState} from "react";
+import Alert from "./components/Alert";
 
 function App() {
   const[mode, setMode] = useState('light');
+  const[alert,setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type ,
+    })
+    setTimeout(()=>{
+      setAlert(null)
+    },1500)
+  }
+
   const toggleMode = ()=>{
     if(mode==="light") {
       setMode("dark")
@@ -20,8 +33,9 @@ function App() {
   return (
     <>
       <Navbar title = "TextUtils" mode= {mode} toggleMode = {toggleMode} />
+      <Alert alert= {alert} />
       <div className="container">
-        <TextForm heading = "Enter the text to analyze below" mode={mode} />
+        <TextForm showAlert= {showAlert} heading = "Enter the text to analyze below" mode={mode} />
         {/* <About /> */}
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,12 +4,15 @@ export default function TextForm(props) {
 
   const upperCaseClick = ()=>{
     setText(text.toUpperCase())
+    props.showAlert("Converted to uppercase","success")
   }
   const lowerCaseClick = ()=>{
     setText(text.toLowerCase())
+    props.showAlert("Converted to lowercase","success")
   }
   const clearClick = ()=>{
     setText("")
+    props.showAlert("Text Cleared","success")
   }
   const speakClick = ()=>{
     let msg = new SpeechSynthesisUtterance(text);
@@ -17,12 +20,14 @@ export default function TextForm(props) {
   }
   const extraSpaceClick = ()=>{
     setText((text.split(/[ ]+/)).join(" "))
+    props.showAlert("Removed all extra spaces","success")
   }
   const replaceClick = ()=>{
     let toReplace = prompt("Enter text to be replaced : ")
-    toReplace = new RegExp(toReplace,'g')
+    let toreplace = new RegExp(toReplace,'g')
     let toReplaceWith = prompt("Enter the text you want to replace with : ")
-    setText(text.replace(toReplace,toReplaceWith))
+    setText(text.replace(toreplace,toReplaceWith))
+    props.showAlert(`Replaced all occourances of ${toReplace} with ${toReplaceWith}`,"success")
   }
   const handleOnChange = (event)=>{
     setText(event.target.value)
@@ -44,8 +49,8 @@ export default function TextForm(props) {
         </div>
         <div className="container my-5" style= {{color : props.mode ==='dark' ? 'white' : '#38303D'}}>
             <h3>Your text summary</h3>
-            <p>{text.split(" ").length } words and {text.length} characters.</p>
-            <p>Average time to read this text -- {0.008 * text.split(" ").length } minutes</p>
+            <p>{text.length >0 ? text.trim().split(" ").length :0 } words and {text.length} characters.</p>
+            <p>Average time to read this text -- {0.008 * (text.length >0 ? text.trim().split(" ").length :0 )} minutes</p>
             <h4>Preview</h4>
             <p>{text}</p>
         </div>
