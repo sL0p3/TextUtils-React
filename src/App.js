2c9cc12e4c7cecd7f8826cff485451a4c5ececa9
sL0p3 <shlokjakhotia@gmail.com>
2022-07-09 14:37:31 +0530

    removing router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import About from "./components/About";
+// import About from "./components/About";
 import React,{useState} from "react";
 import Alert from "./components/Alert";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  // Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   // Link
+// } from "react-router-dom";
 
 function App() {
   const[mode, setMode] = useState('light');
@@ -38,18 +38,18 @@ function App() {
   
   return (
     <>
-    <Router>
+    {/* <Router> */}
       <Navbar title = "TextUtils" mode= {mode} toggleMode = {toggleMode} />
       <Alert alert= {alert} />
       <div className="container">
-        <Routes>
+        {/* <Routes>
           <Route exact path="/about" element= {<About />}></Route>
           <Route exact path="/" element = {<TextForm showAlert= {showAlert} heading = "Enter the text to analyze below" mode={mode}/>}></Route>
-        </Routes>
-        {/* <TextForm showAlert= {showAlert} heading = "Enter the text to analyze below" mode={mode} />
-        <About /> */}
+        </Routes> */}
+        <TextForm showAlert= {showAlert} heading = "Enter the text to analyze below" mode={mode} />
+        {/* <About /> */}
       </div>
-    </Router>
+    {/* </Router> */}
     </>
   );
 }
